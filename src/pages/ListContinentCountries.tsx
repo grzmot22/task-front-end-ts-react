8a4e1e2828fb7a103763bc5399d97cdb4cbda680
Grzmot22 <grzmot22@gmail.com>
2021-06-29 01:00:02 +0200

    Adds styles for ListContinentCountries

--- src/pages/ListContinentCountries.tsx
+++ src/pages/ListContinentCountries.tsx
@@ -1,6 +1,7 @@
 import { useApolloClient, useQuery } from '@apollo/client';
 import React from 'react';
 import { RouteComponentProps } from 'react-router-dom';
+import styled from 'styled-components';
 import Emoji from '../components/Emoji';
 import Loading from '../components/Loading';
 import { GET_CONTINENT_COUNTRIES } from '../gql/continent.gql';
@@ -26,28 +27,67 @@ const ListContinentCountries = ({ match }: RouteComponentProps<TParams>) => {
   }
 
   return (
-    <div>
-      {match.params.code}
-      <div>
+    <>
+      <Container>
+        <Title>{data && data.continent && data.continent.name}</Title>
         {data &&
           data.continent &&
           data.continent.countries.length > 0 &&
           data.continent.countries.map((country) => (
-            <ol key={country.name}>
-              {country.name}
-              <Emoji
+            <ItemContainer key={country.name}>
+              <CountryName>Country: {country.name}</CountryName>
+              {country.languages && country.languages.length > 0 && (
+                <CountryLanguage>
+                  Language: {country.languages[0].name}
+                </CountryLanguage>
+              )}
+              <CountryEmoji
                 key={country.name}
                 label={country.name}
                 symbol={country.emoji}
               />
-              {country.languages && country.languages.length > 0 && (
-                <div>{country.languages[0].name}</div>
-              )}
-            </ol>
+            </ItemContainer>
           ))}
-      </div>
-    </div>
+      </Container>
+    </>
   );
 };
 
 export default ListContinentCountries;
+
+const Container = styled.div`
+  display: grid;
+  height: 100vh;
+  grid-template-rows: 0.2fr 1fr 0.5fr 0.5fr;
+  grid-template-areas:
+    'title title title title'
+    'content content content content';
+  text-align: center;
+  grid-gap: 0.25rem;
+  transition: all 0.25s ease-in-out;
+  @media (max-width: 550px) {
+    grid-template-columns: 1fr;
+    grid-template-rows: 0.4fr 0.4fr 2.2fr 1.2fr 1fr;
+    grid-template-areas:
+      'title'
+      'content'
+      'footer';
+  }
+`;
+
+const Title = styled.title`
+  display: grid;
+  font-weight: bold;
+  font-size: xxx-large;
+  grid-area: title;
+`;
+
+const ItemContainer = styled.div`
+  background: #634fda66;
+  padding: 1.5rem;
+  border-radius: 1rem;
+`;
+
+const CountryName = styled.div``;
+const CountryLanguage = styled.div``;
+const CountryEmoji = styled(Emoji)``;
