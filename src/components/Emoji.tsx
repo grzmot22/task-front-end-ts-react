3ae7b2d7f0bc9eab4aa238f303e20410a0e61d0c
Grzmot22 <grzmot22@gmail.com>
2021-06-28 23:13:13 +0200

    Adds emoji component

---
+++ src/components/Emoji.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+type props = { label: string; symbol: string };
+
+const Emoji = (props: props) => (
+  <span
+    className="emoji"
+    role="img"
+    aria-label={props.label ? props.label : ''}
+    aria-hidden={props.label ? 'false' : 'true'}
+  >
+    {props.symbol}
+  </span>
+);
+
+export default Emoji;
