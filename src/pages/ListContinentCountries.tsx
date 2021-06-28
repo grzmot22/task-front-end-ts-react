f05c4062e5b6c108fcd1f7c38c8ad30d6efc918b
Grzmot22 <grzmot22@gmail.com>
2021-06-28 23:39:11 +0200

    Adds  ListContinentCountries

--- src/pages/ListContinentCountries.tsx
+++ src/pages/ListContinentCountries.tsx
@@ -1,10 +1,53 @@
+import { useApolloClient, useQuery } from '@apollo/client';
 import React from 'react';
 import { RouteComponentProps } from 'react-router-dom';
+import Emoji from '../components/Emoji';
+import Loading from '../components/Loading';
+import { GET_CONTINENT_COUNTRIES } from '../gql/continent.gql';
+import {
+  GetContinentCountries,
+  GetContinentCountriesVariables,
+} from '../gql/types/GetContinentCountries';
 
 type TParams = { code: string };
 
 const ListContinentCountries = ({ match }: RouteComponentProps<TParams>) => {
-  return <div>ListContinentCountries {match.params.code}</div>;
+  const client = useApolloClient();
+  const { data, loading, error } = useQuery<
+    GetContinentCountries,
+    GetContinentCountriesVariables
+  >(GET_CONTINENT_COUNTRIES, {
+    client,
+    variables: { code: match.params.code },
+  });
+
+  if (loading || error) {
+    return error ? <p>error.message </p> : <Loading />;
+  }
+
+  return (
+    <div>
+      {match.params.code}
+      <div>
+        {data &&
+          data.continent &&
+          data.continent.countries.length > 0 &&
+          data.continent.countries.map((country) => (
+            <ol key={country.name}>
+              {country.name}
+              <Emoji
+                key={country.name}
+                label={country.name}
+                symbol={country.emoji}
+              />
+              {country.languages && country.languages.length > 0 && (
+                <div>{country.languages[0].name}</div>
+              )}
+            </ol>
+          ))}
+      </div>
+    </div>
+  );
 };
 
 export default ListContinentCountries;
