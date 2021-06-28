8ebb818ba5af83e37604ae708eb181a6a4128b8f
Grzmot22 <grzmot22@gmail.com>
2021-06-29 01:17:41 +0200

    Adds styles to home page

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,10 +1,35 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import styled from 'styled-components';
 
 const HomePage = () => (
-  <div>
-    Link to <Link to="/continents/">continents</Link>
-  </div>
+  <Container>
+    <Button to="/continents/">Continents</Button>
+  </Container>
 );
 
 export default HomePage;
+
+const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  height: 100vh;
+`;
+
+const Button = styled(Link)`
+  background: #634fda66;
+  opacity: 0.8;
+  border-radius: 1rem;
+  padding: 5px;
+  width: 200px;
+  height: 150px;
+  margin-top: 10px;
+  line-height: 150px;
+  font-weight: bold;
+  color: black;
+  text-align: center;
+  text-decoration: none;
+  box-shadow: 10px 10px 5px 0px rgba(0, 0, 0, 0.75);
+  -webkit-box-shadow: 10px 10px 5px 0px rgba(0, 0, 0, 0.75);
+  -moz-box-shadow: 10px 10px 5px 0px rgba(0, 0, 0, 0.75);
+`;
